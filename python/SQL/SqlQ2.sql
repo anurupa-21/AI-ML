976cdef52b4292814a7531698544cc398779c60c
Anurupa Roy <royanurupa03@gmail.com>
2026-05-09 20:17:24 +0530

    SQL

--- python/SQL/SqlQ2.sql
+++ python/SQL/SqlQ2.sql
@@ -5,7 +5,7 @@ CREATE TABLE ReportingStructure (
     ManagerID INT
 );
 
--- Insert data into the table
+-- Insert data into the table 
 INSERT INTO ReportingStructure (EmployeeID, EmployeeName, ManagerID) VALUES
 (1, 'Alice Smith', NULL),  -- Alice Smith is at the top and reports to no one
 (2, 'Bob Johnson', 1),     -- Bob Johnson reports to Alice Smith
@@ -13,7 +13,9 @@ INSERT INTO ReportingStructure (EmployeeID, EmployeeName, ManagerID) VALUES
 (4, 'David Brown', 2),     -- David Brown reports to Bob Johnson
 (5, 'Eve Davis', 2),       -- Eve Davis reports to Bob Johnson
 (6, 'Frank Miller', 3);    -- Frank Miller reports to Carol White
+   
 
+  
 
 select * from ReportingStructure
 
